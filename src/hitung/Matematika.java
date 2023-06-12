318c2ed0ee537600da113f41fd38563d484acf58
valdi02 <mvaldierlangga1@gmail.com>
2023-06-12 16:06:17 +0700

    membuat constructor dan setPenjumlahan

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -10,5 +10,18 @@ package hitung;
  * @author valdi
  */
 public class Matematika {
+    //membuat variable
+    private double bil1,bil2;
     
+    //membuat constructor
+    public Matematika(double bil1, double bil2){
+         this.bil1 = bil1;
+         this.bil2 = bil2;
+    }
+    
+    //membuat method setPenjumlahan
+    double setPenjumlahan() {
+        return bil1 + bil2;
+    }
+ 
 }
